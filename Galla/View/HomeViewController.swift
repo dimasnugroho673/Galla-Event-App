11c2bd046539dd029d03a9c71c58794d0434417e
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2022-06-22 16:54:30 +0700

    complete search uitextfield

--- Galla/View/HomeViewController.swift
+++ Galla/View/HomeViewController.swift
@@ -36,16 +36,6 @@ class HomeViewController: UIViewController {
     return label
   }()
 
-  lazy var myLocationLabel: UILabel = {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    label.text = "Bandung, ID"
-    label.font = UIFont(name: "Poppins-SemiBold", size: 20)
-    label.textColor = UIColor(named: "color-black")
-
-    return label
-  }()
-
   lazy var myLocationHeader: UIView = {
     let view = UIView()
     view.translatesAutoresizingMaskIntoConstraints = false
@@ -84,6 +74,38 @@ class HomeViewController: UIViewController {
     return img
   }()
 
+  lazy var placeholderSearchLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = "Search all events..."
+    label.font = UIFont(name: "Poppins-Regular", size: 14)
+    label.textColor = .systemGray2
+
+    return label
+  }()
+
+  lazy var placeholderSearchImage: UIImageView = {
+    let img = UIImageView()
+    img.translatesAutoresizingMaskIntoConstraints = false
+    img.image = UIImage(named: "icon-search")
+    img.widthAnchor.constraint(equalToConstant: 18).isActive = true
+    img.heightAnchor.constraint(equalToConstant: 18).isActive = true
+
+    return img
+  }()
+
+  lazy var searchTextField: UITextField = {
+    let tf = UITextField()
+
+    tf.translatesAutoresizingMaskIntoConstraints = false
+    tf.backgroundColor = UIColor.white
+    tf.heightAnchor.constraint(equalToConstant: 54).isActive = true
+    tf.layer.cornerRadius = 12
+    tf.font = UIFont(name: "Poppins-Regular", size: 14)
+
+    return tf
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -98,8 +120,18 @@ class HomeViewController: UIViewController {
     myLoationStack.axis = .vertical
     myLoationStack.spacing = 3
 
+    let searchPlaceholderStack = UIStackView(arrangedSubviews: [placeholderSearchImage, placeholderSearchLabel])
+    searchPlaceholderStack.translatesAutoresizingMaskIntoConstraints = false
+    searchPlaceholderStack.axis = .horizontal
+    searchPlaceholderStack.spacing = 15
+
     containerView.addSubview(myLoationStack)
     containerView.addSubview(profileImage)
+    containerView.addSubview(searchTextField)
+    searchTextField.addSubview(searchPlaceholderStack)
+    searchTextField.setLeftPaddingPoints(45)
+
+    NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextField.textDidChangeNotification, object: nil)
 
     NSLayoutConstraint.activate([
       scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
@@ -118,10 +150,35 @@ class HomeViewController: UIViewController {
       myLoationStack.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 15),
 
       profileImage.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 10),
-      profileImage.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -15)
+      profileImage.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -15),
+
+      searchTextField.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20),
+      searchTextField.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 15),
+      searchTextField.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -15),
+
+      searchPlaceholderStack.centerYAnchor.constraint(equalTo: searchTextField.centerYAnchor, constant: 0),
+      searchPlaceholderStack.leftAnchor.constraint(equalTo: searchTextField.leftAnchor, constant: 13)
     ])
+  }
+
+  @objc func handleTextInputChange() {
+    placeholderSearchLabel.isHidden = !searchTextField.text!.isEmpty
   }
 
 
 
+}
+
+
+extension UITextField {
+  func setLeftPaddingPoints(_ amount:CGFloat){
+    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+    self.leftView = paddingView
+    self.leftViewMode = .always
+  }
+  func setRightPaddingPoints(_ amount:CGFloat) {
+    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+    self.rightView = paddingView
+    self.rightViewMode = .always
+  }
 }
